26693095f5cc52bc87042ba7847cb9ac5d1895fb
Autumn Valenta <autumn@discoverywritten.com>
2025-09-22 02:18:59 -0600

    Update QR
    We'll use an SVG for something probably, but the diff is annoying to look at

--- bin/make-qr.sh
+++ bin/make-qr.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 gzip -9 -c docs/_site_qr/index.html > docs/_includes/index.bin
-qrencode -t SVG -s 1 -m 0 -l M -o docs/qr.svg < docs/_includes/index.bin
+qrencode -s 3 -m 1 -l M -o docs/qr.png < docs/_includes/index.bin
