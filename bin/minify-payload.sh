c4d5432d82b955cc527f9a311c18c60a5cb12cfb
Autumn Valenta <autumn@discoverywritten.com>
2025-09-23 00:52:26 -0600

    Add b64 fingerprinting
    We dropped this in our haste to unify base64 under qrencoder/qrcode, but this base64 wasn't supposed to leave.

--- bin/minify-payload.sh
+++ bin/minify-payload.sh
@@ -4,13 +4,15 @@ IN=docs/_data/payload.js
 PEM=docs/_includes/public.b64
 URL="https://anecdote\.discoverywritten\.com"
 if [ "$1" = "--qr" ]; then
-  TARGET=docs/_includes/payload.qr.min.js
+  NAME=payload.qr.min
+  TARGET=docs/_includes/$NAME.js
 else
-  TARGET=docs/_includes/payload.min.js
-  if [ "$1" = "--dev" ]; then
-    URL="[[ You shouldn't use the canonical, just start with / ]]"  # this value shouldn't be rendered in this path
-  fi
+  NAME=payload.min
+  TARGET=docs/_includes/$NAME.js
+  URL=""
 fi
+
+B64=docs/_includes/$NAME.b64
 
 # Minify
 node_modules/.bin/terser "$IN" -c passes=3,drop_console,unsafe_arrows,booleans_as_integers \
@@ -24,5 +26,7 @@ sed -i.bak "s@{% include public\.b64 %}@$(<"$PEM")@" "$TARGET" && rm "$TARGET.ba
 # We have to replace this because we're using a file jekyll hasn't rendered yet.
 # We do this to avoid querying it out of the docs/_site/index.html tag,
 # but it could be done.
-echo "Replacing {{ site.canonical }} with manifest provider: ${URL:-<self>}"
-sed -i.bak "s@{{ site\.canonical }}@$URL@g" "$TARGET" && rm "$TARGET.bak"
+echo "Replacing {{ site.remote }} with manifest provider: ${URL:-<self>}"
+sed -i.bak "s@{{ site\.remote }}@$URL@g" "$TARGET" && rm "$TARGET.bak"
+
+openssl dgst -sha256 -binary "$TARGET" | openssl base64 -A > "$B64"
