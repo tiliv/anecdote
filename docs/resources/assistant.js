b0b715a6c1b5d437fc922216cd304d0f4a7f360f
Autumn Valenta <autumn@discoverywritten.com>
2025-09-24 22:41:26 -0600

    Stub Assistant

---
+++ docs/resources/assistant.js
@@ -0,0 +1,51 @@
+import { pipeline, env } from '@xenova/transformers';
+
+env.allowLocalModels = true;
+env.useBrowserCache = false;
+
+class Assistant {
+  static meta = {
+    "protocol": "2025-09-24",
+  };
+  static instance = null;
+  static use() {
+    if (this.instance === null) {
+      // This pipeline should intercept url-namespaces for model loads, and
+      // handle any novel fetch processes, and serve it as the source.
+      // The manifest's model names work as canonical paths, just relative;
+      // They can be prefixed with other protocols.
+      this.instance = pipeline(this.task, this.model, {
+        progress_callback: (msg) => self.postMessage(msg),
+      });
+    }
+    return this.instance;
+  }
+
+  async reduce(txt) {
+    // TODO: Orchestrate model plan for this task.
+    // We are doing it well when we know what models we want but haven't cached.
+    return {}
+  }
+
+  tasks = {
+    summarization: reduce,
+  };
+}
+
+self.addEventListener('message', async (event) => {
+  const { assistant } = event.data || {};
+  if (!assistant) return;
+  const { warmUp, ...task } = assistant;
+  const A = Assistant.use();
+  if (warmUp) return;
+  const { name, version, prompt, ...input } = task;
+  const reply = out => self.postMessage({
+    status: 'analyzed',
+    output: {
+      name, version: version + 1,
+      ...JSON.parse(JSON.stringify(out))
+    },
+  });
+
+  reply({ NotImplemented: A.meta });
+});
