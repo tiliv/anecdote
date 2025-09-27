3e77ca6b21a8ea50789f7113438490a020cbe39a
Autumn Valenta <autumn@discoverywritten.com>
2025-09-27 00:43:26 -0600

    Fix content-type for specific loads
    This is kind of sweeping, but it allows us to map our types to content-type when it matters to the browser for how it loads or triggers internal resources. The most notable example is text/html loaded as an application/manifest+html for the widget system. A strange vendor content-type makes accessing the url a content-disposition:attachment, so iframing it is not possible.

--- docs/_data/payload.js
+++ docs/_data/payload.js
@@ -1,7 +1,19 @@
-const BUNDLE = 'application/manifest+json';
+const LOADED = new Promise(y => {
+  if (document.readyState !== 'loading') return y();
+  document.addEventListener('DOMContentLoaded', () => y(), { once: true });
+});
 const WORKER = 'application/vnd.anecdote.worker';
+const BUNDLE = 'application/manifest+json';
+const WIDGET = 'application/manifest+html';
 const SCRIPT = 'application/javascript';
+const MODULE = 'module';
 const STYLES = 'text/css';
+const MAPPED = {  // Content-Types used in Files that hold these blobs
+  [WORKER]: MODULE,
+  [MODULE]: SCRIPT,
+  [BUNDLE]: 'application/json',
+  [WIDGET]: 'text/html',
+};
 
 (async function(){
   const _fetch = url => fetch(url, { cache: 'no-store', credentials: 'omit', mode: 'cors' });
@@ -12,64 +24,67 @@ const STYLES = 'text/css';
     const text = await response.text();
     const obj = JSON.parse(text);
     await manifest(obj);
-    async function manifest({
-      meta: { node },
-      candidates={}, candidate_order=[],
-      resources={}, resource_order=[]
-    }) {
-      const workers = { type: 'module', scope: candidates.dns };
-      const after = {
-        [WORKER]: ({ uri }) => navigator.serviceWorker?.register?.(node + uri, workers),
-        [BUNDLE]: async ({ file }) => manifest(JSON.parse(await file.text())),
-      };
-
-      // This function might need to do medium.js things to retrieve content.
-      // We'd use storage uris instead of blobs for files.
+    async function manifest({ meta: { node }, candidates={}, resources={} }) {
       async function resource({ strategy, uri, path, type }){
+        let _type = MAPPED[type] ?? type;
+
         async function fill() {
+          let resource = null;
           switch (strategy) {
-            case 'dns':
-              const resource = await _fetch(node + uri);
-              return new Blob([await resource.arrayBuffer()], { type });
+            default: throw new Error(strategy);
+            case 'dns': resource = await _fetch(node + uri); break;
           }
+          return new Blob([await resource.arrayBuffer()], { type });
         }
-        const file = new File([await fill()], path, { type });
-        let url = null;
-        let attr = null;
+
+        const file = new File([await fill()], path, { type: _type });
+        const fix = (el, o) => Object.entries(o).forEach(([k,v]) => el[k] = v);
+        let [url, attr] = [null, null];
         switch (type) {
-          case BUNDLE:
-          case WORKER:
-          case SCRIPT: [kind, attr] = ['script', 'src']; break;
           default: [kind, attr] = ['link', 'href']; break;
+          case WORKER: case BUNDLE: case MODULE:
+          case SCRIPT: [kind, attr] = ['script', 'src']; break;
+          case WIDGET: [kind, attr] = ['iframe', 'src']; break;
         }
         const el = document.createElement(kind);
         switch (type) {
-          case WORKER: url = node + uri; // no break
-          case SCRIPT: el.type = 'module'; break;
-          case BUNDLE: el.type = type; break;
-          case STYLES: el.rel = 'stylesheet'; break;
-          default: break;
+          case WORKER: case BUNDLE: fix(el, { type: _type }); break;
+          default: fix(el, { type }); break;
+        }
+        switch (type) {
+          case WORKER: url = node + uri; break;
+          case WIDGET: fix(el, { sandbox: 'allow-scripts allow-same-origin' }); break;
+          case STYLES: fix(el, { rel: 'stylesheet' }); break;
         }
         if (!url) url = URL.createObjectURL(file);
         el[attr] = url;
         el.id = path;
-        document.head.appendChild(el);
-        return { strategy, uri, id: path, type, file, url };
+        await LOADED;
+        switch (type) {
+          case WIDGET: document.body.appendChild(el); break;
+          default: document.head.appendChild(el); break;
+        }
+
+        // console.log("Loaded", strategy, uri, type, file.type, file.size, el);
+
+        return (async () => { switch (type) {
+          default: return Promise.resolve(el);
+          case WORKER: return navigator.serviceWorker?.register?.(node + uri, { type: _type, scope: candidates.dns });
+          case BUNDLE: return manifest(JSON.parse(await file.text()));
+        }})();
       }
 
-      for (const label of resource_order) {
+      for (const label in resources) {
         if (!resources[label]) continue;
-        for (const strategy of candidate_order) {
+        const promises = [];
+        for (const strategy in candidates) {
           if (!(strategy in candidates)) continue;
           const { [strategy]: path=label } = resources[label];
-          const meta = await resource({
-            ...resources[label],
-            uri: candidates[strategy] + path,
-            path,
-            strategy
-          });
-          /* await */ after[meta.type]?.(meta);
+          const uri = candidates[strategy] + path;
+          const p = resource({ ...resources[label], uri, path, strategy });
+          promises.push(p);
         }
+        await Promise.all(promises);
       }
     }
   }
