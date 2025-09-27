9cacf15dfde31ce60e582319bf300393769ff872
Autumn Valenta <autumn@discoverywritten.com>
2025-09-26 23:04:26 -0600

    Don't minify if in --dev

--- bin/minify-payload.sh
+++ bin/minify-payload.sh
@@ -15,9 +15,13 @@ fi
 B64=docs/_includes/$NAME.b64
 
 # Minify
-node_modules/.bin/terser "$IN" -c passes=3,drop_console,unsafe_arrows,booleans_as_integers \
-  -m toplevel --toplevel --format wrap_iife,ecma=2020 \
-  -o "$TARGET"
+if [ "$1" = "--qr" ]; then
+  node_modules/.bin/terser "$IN" -c passes=3,unsafe_arrows,booleans_as_integers \
+    -m toplevel --toplevel --format wrap_iife,ecma=2020 \
+    -o "$TARGET"
+else
+  cp "$IN" "$TARGET"
+fi
 
 # # Naive {% include %}, avoid / delimiter in regex because it's valid base64 too
 # echo "Replacing {% include public.b64 %} with key $PEM"
